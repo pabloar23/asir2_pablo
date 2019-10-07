30380d9bbe69683d1bffa097eb9e8bd83748492b
unknown <pablo.argumosa.ruiz@gmail.com>
2019-10-07 16:21:50 +0200

    Actualizar

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
-if(isset($_GET))
-	print_r($_GET);
-
+if(isset($_GET['email']))
+print_r($_GET);
+$f=fopen('Suscriptores.txt','a');
+fwrite($f,$_GET['email']."\r\n");
+fclose($f)
 ?>
 
 <form>
-	<input/>
+	<input name="email" type="email"/>
 	<button>Suscribirse</button>
 </form>
