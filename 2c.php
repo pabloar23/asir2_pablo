7e77b2807b7f0c37b3f67adf9ad2af9fec84395e
unknown <pablo.argumosa.ruiz@gmail.com>
2019-10-14 15:30:17 +0200

    Parametros

---
+++ 2c.php
@@ -0,0 +1,22 @@
+<?php
+function operaciones($a,$b){
+	/*
+	$a=5;
+	$b=6;
+	*/
+	echo 'Primer número='.$a;
+	echo '</br>';
+	echo 'Segundo número='.$b;
+	echo '</br>';
+	echo '</br>';
+	echo 'Suma: '.$a.'+'.$b.'='.($a+$b);
+	echo '</br>';
+	echo 'Resta: '.$a.'-'.$b.'='.($a-$b);
+	echo '</br>';
+	echo 'Multiplicación: '.$a.'x'.$b.'='.($a*$b);
+	echo '</br>';
+	echo 'División: '.$a.'/'.$b.'='.($a/$b);
+	echo '</br>';
+}
+operaciones(5,6);
+?>
