da24b4a762037dd80464ac30c5e109e4e68293d8
unknown <pablo.argumosa.ruiz@gmail.com>
2019-10-14 16:26:55 +0200

    Condicionales

---
+++ 2d.php
@@ -0,0 +1,29 @@
+<?php
+function operaciones($a,$b){
+	/*
+	$a=5;
+	$b=6;
+	*/
+	$r=null;
+	$r=$r.'Primer número='.$a;
+	$r=$r.'</br>';
+	$r=$r.'Segundo número='.$b;
+	$r=$r.'</br>';
+	$r=$r.'</br>';
+	$r=$r.'Suma: '.$a.'+'.$b.'='.($a+$b);
+	$r=$r.'</br>';
+	$r=$r.'Resta: '.$a.'-'.$b.'='.($a-$b);
+	$r=$r.'</br>';
+	$r=$r.'Multiplicación: '.$a.'x'.$b.'='.($a*$b);
+	$r=$r.'</br>';
+	if($b!=6){
+		$r=$r.'División: '.$a.'/'.$b.'='.($a/$b);
+		$r=$r.'</br>';
+	}
+	return $r;
+}
+$s=operaciones(5,6);
+echo $s;
+echo operaciones(7,6);
+
+?>
