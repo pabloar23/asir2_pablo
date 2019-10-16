365faab443023354b485a3221e48585403941aaf
pabloar23 <pablo.argumosa.ruiz@gmail.com>
2019-10-16 10:30:51 +0200

    Funciones

--- 2b.php
+++ 2b.php
@@ -1,11 +1,7 @@
 <?php
-<<<<<<< HEAD
-
 $a=5;
 $b=2;
-function calculadora($a,$b){
-echo 'suma '. 'a' '+' 'b'
-=======
+
 function operaciones(){
 	$a=5;
 	$b=6;
@@ -25,4 +21,3 @@ function operaciones(){
 }
 operaciones();
 ?>
->>>>>>> 7c4d78b857dccc398e75aeba7dcd2e6a25316386
