cde929a14ac65a259520f35e2a8ce922b2cb7b96
pabloar23 <pablo.argumosa.ruiz@gmail.com>
2019-11-08 19:59:57 +0100

    Else A

---
+++ html.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+		<title>LAS MARCAS MAS CARAS</title>
+		
+</head>
+<body bgcolor="#E6E6FA">
+
+			<h1>Gucci</h1>
+				<p><b>Gucci</b> es una firma de lujo <b><u>italiana fundada en 1921</b></u> dedicada al <b><u>diseño y fabricación de artículos de moda, maletas, relojes, perfumes, etc.</u></b></br>
+			<img
+			src="https://i1.wp.com/www.america-retail.com/static//2018/10/gucci.png?fit=1170%2C700&ssl=1"
+			align=left
+			width=150
+			height=100
+			/>
+			<table height="150"><tr><td></td></tr></table>
+			<h1>Louis Vuitton</h1>
+				<p><b>Louis Vuitton</b> casa <b><u>fundada en 1854</b></u>, es una empresa <b><u>francesa</b></u> de marroquinería de lujo <b><u>especializada en artículos de viaje que vende a través de boutiques independientes, departamentos en almacenes de alta gama y a través de la sección de comercio electrónico en su sitio web.</b></u></p>
+			<img
+			src="https://thumbor.forbes.com/thumbor/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ce2e16b169cc600076c55c4%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D416%26cropY1%3D0%26cropY2%3D416"
+			align=left
+			width=150
+			height=100
+			/>
+			<table height="150"><tr><td></td></tr></table>
+			<h1>Versace</h1>
+				<p><b>Versace</b>, es una <b><u>casa de moda Italiana, fundada por Gianni Versace en 1978.</b></u>
+			<table height="10"><tr><td></td></tr></table>
+			<img
+			src="versace.jpg"
+			align=left
+			width=150
+			height=100
+			/>
+			<table height="150"><tr><td></td></tr></table>
+			
+			<form action="ejemplo.php" method="get">
+				<p>Nombre de la marca: <input type="text" name="nombre" size="40"></p>
+				<p>Fecha de creación: <input type="number" name="nacido" min="1800"></p>
+				<p>País: <input type="radio" name="hm" value="h"> Francia <input type="radio" name="hm" value="m"> Italia
+				</p>
+				<p>
+			<input type="submit" value="Enviar">
+			<input type="reset" value="Borrar">
+				</p>
+			</form>
+			</br>
+			<table border=1>
+			<center>
+				<tr>
+					<th>Marca</th>
+					<th>País</th>
+				</tr>
+				<tr>
+					<td>Gucci</td>
+					<td>Italia</td>	
+				</tr>
+				<tr>
+					<td>Louis Vuitton</td>
+					<td>Francia</td>
+				</tr>
+				<tr>
+					<td>Versace</td>
+					<td>Italia</td>
+			</center>
+			</table>
+					
+					
+					
+			
+</body>
+			
+			
+		
+		
+</html>
