45bc1a91e5ad7e13759b83fa8be09203145c125f
unknown <pablo.argumosa.ruiz@gmail.com>
2019-11-11 16:38:41 +0100

    actualizando

--- html.php
+++ html.php
@@ -5,34 +5,53 @@
 </head>
 <body bgcolor="#E6E6FA">
 
-			<h1>Gucci</h1>
+
+		<center>
+		<h1>Las marcas de moda más caras<h1>
+		<hr>
+			<h2>Gucci</h2>
 				<p><b>Gucci</b> es una firma de lujo <b><u>italiana fundada en 1921</b></u> dedicada al <b><u>diseño y fabricación de artículos de moda, maletas, relojes, perfumes, etc.</u></b></br>
+						<table height="75"><tr><td></td></tr></table>
+			<h4>Lo más buscado</h4>
+				<p>Lo más buscado en nombre de la marca Gucci son bolsos, calcetines, camisetas y mochilas.</p>
 			<img
 			src="https://i1.wp.com/www.america-retail.com/static//2018/10/gucci.png?fit=1170%2C700&ssl=1"
-			align=left
+			align=center
 			width=150
 			height=100
 			/>
-			<table height="150"><tr><td></td></tr></table>
-			<h1>Louis Vuitton</h1>
+		</center>
+		<table height="75"><tr><td></td></tr></table>
+		<hr>
+		<center>
+			<h2>Louis Vuitton</h2>
 				<p><b>Louis Vuitton</b> casa <b><u>fundada en 1854</b></u>, es una empresa <b><u>francesa</b></u> de marroquinería de lujo <b><u>especializada en artículos de viaje que vende a través de boutiques independientes, departamentos en almacenes de alta gama y a través de la sección de comercio electrónico en su sitio web.</b></u></p>
+				<table height="75"><tr><td></td></tr></table>
+			<h4>Lo más vendido</h4>
+				<p>Lo más vendido en nombre de la marca Louis Vuitton son bolsos, maletas y carteras.</p>
 			<img
 			src="https://thumbor.forbes.com/thumbor/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ce2e16b169cc600076c55c4%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D416%26cropY1%3D0%26cropY2%3D416"
-			align=left
+			align=center
 			width=150
 			height=100
 			/>
-			<table height="150"><tr><td></td></tr></table>
-			<h1>Versace</h1>
+		</center>
+			<table height="75"><tr><td></td></tr></table>
+			<hr>
+		<center>
+			<h2>Versace</h2>
 				<p><b>Versace</b>, es una <b><u>casa de moda Italiana, fundada por Gianni Versace en 1978.</b></u>
-			<table height="10"><tr><td></td></tr></table>
+			<table height="75"><tr><td></td></tr></table>
+			<h4>Famosos que visten la marca</h4>
+			<p>Entre los famosos que visten la marca Versace frecuentemente se encuentran Anuel AA, Irina Shayk y Rosalía.</p>
 			<img
 			src="versace.jpg"
-			align=left
+			align=center
 			width=150
 			height=100
 			/>
-			<table height="150"><tr><td></td></tr></table>
+			<table height="75"><tr><td></td></tr></table>
+			<hr>
 			
 			<form action="ejemplo.php" method="get">
 				<p>Nombre de la marca: <input type="text" name="nombre" size="40"></p>
@@ -72,5 +91,5 @@
 			
 			
 		
-		
+<font size="6"		
 </html>
---
+++ triangulos_c.php
@@ -0,0 +1,75 @@
+<form>
+	Ángulo 1    
+	<input name="angulo1"/>
+	</br>
+	Ángulo 2    
+	<input name="angulo2"/>
+	</br>
+	Ángulo 3  
+	<input name="angulo3"/>
+	</br>
+	<button>Comprobar</button>
+	</br>
+	</br>
+</form>
+
+<form>
+	<button href="http://localhost/asir2_alvaro/triangulos_c.php">Poner a 0 los valores.</button>
+	</br>
+	</br>
+</form>
+
+<?php
+function triangulo($a,$b,$c,$A,$B,$C){
+	
+	$a=$A;
+	$b=$B;
+	$c=$C;
+	
+	echo lados($a,$b,$c);
+	echo angulos($A,$B,$C);
+	
+}
+function lados($a,$b,$c){
+	
+	if(is_numeric($a) && is_numeric($b) && is_numeric($c) && ($a)>0 && ($b)>0 && ($c)>0){
+			if($a==$b && $a==$c){
+				$r='Triángulo equilátero ';
+			}elseif($a==$b || $a==$c || $b==$c){
+				$r='Triángulo isósceles ';
+			}else
+				$r='Triángulo escaleno ';
+			return $r;
+				
+	}
+}
+function angulos($A,$B,$C){
+	if(is_numeric($A) && is_numeric($B) && is_numeric($C) && ($A)>0 && ($B)>0 && ($C)){
+		if((($A)+($B)+($C))==180){
+			if($A==90 || $B==90 || $C==90){
+				$r='y rectángulo.';
+			}elseif($A>90 || $B>90 || $C>90){
+				$r='y obtusángulo.';
+			}else
+				$r='y acutángulo.';
+			return $r;
+			
+		}else
+			echo 'Los ángulos de un triángulo han de sumar 180º, revisa que has metido.';
+	}else
+		echo 'Introduce valores numéricos, por favor.';
+	
+}
+if(isset($_GET['angulo1'], $_GET['angulo2'], $_GET['angulo3'])){
+	
+	echo triangulo(1,1,1,($_GET['angulo1']),($_GET['angulo2']),($_GET['angulo3']));
+	
+}else
+	echo 'Introduce la medida de los angulos del triángulo.';
+echo '<br><br>Vectores de test:<br><br>';
+echo triangulo(1,sqrt(2),1,45,90,45).' ¿rectángulo-isósceles?<br/>';
+echo triangulo(1,sqrt(2),1,45,45,90).' ¿rectángulo-isósceles?<br/>';
+echo triangulo(1,sqrt(2),1,90,45,45).' ¿rectángulo-isósceles?<br/>';
+echo triangulo(1,1,1,60,60,60).' ¿acutángulo-equilátero?<br/>';
+echo triangulo(1,1,1,100,40,40).' ¿obtusángulo-isósceles?<br/>';
+?>
