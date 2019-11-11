05ae80f9a9b467264161349c54ce4323a77e52a2
unknown <pablo.argumosa.ruiz@gmail.com>
2019-11-11 17:46:10 +0100

    actualizando2

--- html.php
+++ html.php
@@ -6,84 +6,74 @@
 <body bgcolor="#E6E6FA">
 
 
+
 		<center>
-		<h1>Las marcas de moda más caras<h1>
+		<h1>Las marcas de moda más caras</h1>
 		<hr>
 			<h2>Gucci</h2>
 				<p><b>Gucci</b> es una firma de lujo <b><u>italiana fundada en 1921</b></u> dedicada al <b><u>diseño y fabricación de artículos de moda, maletas, relojes, perfumes, etc.</u></b></br>
 						<table height="75"><tr><td></td></tr></table>
-			<h4>Lo más buscado</h4>
+			<h3>Lo más buscado</h3>
 				<p>Lo más buscado en nombre de la marca Gucci son bolsos, calcetines, camisetas y mochilas.</p>
 			<img
 			src="https://i1.wp.com/www.america-retail.com/static//2018/10/gucci.png?fit=1170%2C700&ssl=1"
-			align=center
-			width=150
-			height=100
+			align="center"
+			width="150"
+			height="100"
 			/>
 		</center>
 		<table height="75"><tr><td></td></tr></table>
-		<hr>
+		<hr/>
 		<center>
 			<h2>Louis Vuitton</h2>
 				<p><b>Louis Vuitton</b> casa <b><u>fundada en 1854</b></u>, es una empresa <b><u>francesa</b></u> de marroquinería de lujo <b><u>especializada en artículos de viaje que vende a través de boutiques independientes, departamentos en almacenes de alta gama y a través de la sección de comercio electrónico en su sitio web.</b></u></p>
 				<table height="75"><tr><td></td></tr></table>
-			<h4>Lo más vendido</h4>
+			<h3>Lo más vendido</h3>
 				<p>Lo más vendido en nombre de la marca Louis Vuitton son bolsos, maletas y carteras.</p>
 			<img
 			src="https://thumbor.forbes.com/thumbor/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ce2e16b169cc600076c55c4%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D416%26cropY1%3D0%26cropY2%3D416"
-			align=center
-			width=150
-			height=100
+			align="center"
+			width="150"
+			height="100"
 			/>
 		</center>
 			<table height="75"><tr><td></td></tr></table>
-			<hr>
+			<hr/>
 		<center>
 			<h2>Versace</h2>
 				<p><b>Versace</b>, es una <b><u>casa de moda Italiana, fundada por Gianni Versace en 1978.</b></u>
 			<table height="75"><tr><td></td></tr></table>
-			<h4>Famosos que visten la marca</h4>
+			<h3>Famosos que visten la marca</h3>
 			<p>Entre los famosos que visten la marca Versace frecuentemente se encuentran Anuel AA, Irina Shayk y Rosalía.</p>
 			<img
 			src="versace.jpg"
-			align=center
-			width=150
-			height=100
+			align="center"
+			width="150"
+			height="100"
 			/>
 			<table height="75"><tr><td></td></tr></table>
-			<hr>
+			<hr/>
 			
-			<form action="ejemplo.php" method="get">
-				<p>Nombre de la marca: <input type="text" name="nombre" size="40"></p>
-				<p>Fecha de creación: <input type="number" name="nacido" min="1800"></p>
-				<p>País: <input type="radio" name="hm" value="h"> Francia <input type="radio" name="hm" value="m"> Italia
-				</p>
-				<p>
-			<input type="submit" value="Enviar">
-			<input type="reset" value="Borrar">
-				</p>
+		
+		<p>Le dejamos a continuación un formulario donde si se identifica con su <b>ID Cliente y su clave correspondiente</b> podrá iniciar sesión y valorar su experiencia en nuestra web, gracias de antemano.</p>
+		
+			<form>
+				ID Cliente:<br/>
+			<input type="text" name="username"><br/>
+				Clave:<br/>
+			<input type="password" name="psw"><br/>
+				Utilizar estas credenciales para iniciar sesión:<br/>
+			<input type="submit" value="Submit"><br/>
 			</form>
-			</br>
-			<table border=1>
+			<hr/>
+			
 			<center>
-				<tr>
-					<th>Marca</th>
-					<th>País</th>
-				</tr>
-				<tr>
-					<td>Gucci</td>
-					<td>Italia</td>	
-				</tr>
-				<tr>
-					<td>Louis Vuitton</td>
-					<td>Francia</td>
-				</tr>
-				<tr>
-					<td>Versace</td>
-					<td>Italia</td>
-			</center>
-			</table>
-					
+			<table width="80%" border >
+			<tr><td colspan="2" text align=center bgcolor="white"><b>Creado y editado por ASIR2Decroly S.L</b></td></tr>
+			<tr><td text align=center bgcolor="white"><b>Versión 8.4</b></td><td text align=center bgcolor="white" ><b>All rights reserved</b></td></tr>
+</table>
+</center>
+				
 					
 					
 			
@@ -91,5 +81,5 @@
 			
 			
 		
-<font size="6"		
+	
 </html>
